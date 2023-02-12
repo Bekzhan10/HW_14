04fdc56ba3d0f2eaa06f35b591dea38d4b55a231
Bekzhan10 <90493468+Bekzhan10@users.noreply.github.com>
2023-02-13 02:41:50 +0600

    HW_14

---
+++ src/test/java/dar/university/backend/PostDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dar.university.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
